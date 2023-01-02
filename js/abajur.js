1afaf4f6c5e3e33dca2b0f9ec7e6ab234c91be18
Rodrigo Menezes <rodrusantu7@gmail.com>
2023-01-02 18:57:19 -0300

    Melhoria

--- js/abajur.js
+++ js/abajur.js
@@ -1,18 +1,18 @@
+
 const img = document.getElementById('imagem');
+const resposta = document.getElementById('resposta');
+
 
 const abajur = (e) => {
-    turnOn[e.target.id]();
-    var resposta = document.getElementById('resposta');
+    imagens[e.target.id]();
     resposta.innerHTML = `A sua sorte está lançada você escolheu a COR da "sorte"...`
 }
 
-
-const turnOn = {
+const imagens = {
     'red':      () => img.src = './img/vermelho.png',
     'yellow':   () => img.src = './img/amarelo.png',
     'green':    () => img.src = './img/verde.png',
 }
 
 buttons.addEventListener('click', abajur);
-
 
